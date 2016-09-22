d337d5e6b4424ee79fe8b230202e58c76dffe96a
Graham Laidler <graham.laidler@harbourassist.com>
2016-09-22 11:05:42 +0100

    initial trial commit

---
+++ ErrorContainer.ts
@@ -0,0 +1,20 @@
+﻿class ErrorContainer {
+    constructor() {
+        this.clear();
+		this.failed = false;
+    }
+	failed:boolean;
+    addFatalResponse(r: any) {
+        var msg = (r.data && r.data.responseStatus) ? r.data.responseStatus.message : r.statusText || r;
+		this.failed = true;
+        this.fatals.push(msg);
+    };
+
+    fatals: string[];
+    warnings: string[];
+
+    clear() {
+        this.fatals = [];
+        this.warnings = [];
+    }
+}
---
+++ Toolbars.ts
@@ -0,0 +1,10 @@
+﻿
+interface IToolbarButton {
+    name: string;
+    subActions?: IToolbarButton[];
+    icon?: string;
+    btnClass?: string;
+    func?: (item: any) => void;
+    disabled?: (item: any) => boolean;
+    hidden?: (item: any) => boolean;
+}
