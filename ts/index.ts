863ad68a01f8d6e5ee430468d30d8071f8aa6715
Graham Laidler <graham.laidler@harbourassist.com>
2016-09-22 11:55:43 +0100

    put in one file for the sake of time

--- ts/index.ts
+++ ts/index.ts
@@ -1,2 +1,31 @@
-export * from "./ui/IToolbar";
-export * from "./errors/ToolbarButton";
+namespace tsUtils {
+    export interface IToolbarButton {
+        name: string;
+        subActions?: IToolbarButton[];
+        icon?: string;
+        btnClass?: string;
+        func?: (item: any) => void;
+        disabled?: (item: any) => boolean;
+        hidden?: (item: any) => boolean;
+    }
+    export class ErrorContainer {
+        constructor() {
+            this.clear();
+            this.failed = false;
+        }
+        failed: boolean;
+        addFatalResponse(r: any) {
+            var msg = (r.data && r.data.responseStatus) ? r.data.responseStatus.message : r.statusText || r;
+            this.failed = true;
+            this.fatals.push(msg);
+        };
+
+        fatals: string[];
+        warnings: string[];
+
+        clear() {
+            this.fatals = [];
+            this.warnings = [];
+        }
+    }
+}
